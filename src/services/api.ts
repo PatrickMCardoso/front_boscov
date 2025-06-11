472d72e9ae58f5c1f9e7c01fbee32976262ca66c
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-06-11 15:58:47 -0300

    Removendo user e toker do LocalStorage após erro 401

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // URL do backend
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3030",
 });
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/not-authenticated";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
